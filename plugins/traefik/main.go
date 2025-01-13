8a1f4c7d4e960e11e799ec1aaec72ec515183897
Geo Saglio <geosaglioo@gmail.com>
2025-01-13 10:25:33 +0100

    feat(traefik plugin) add logs to middleware

--- plugins/traefik/main.go
+++ plugins/traefik/main.go
@@ -1,6 +1,7 @@
 package traefik
 
 import (
+	"fmt"
 	"bufio"
 	"context"
 	"fmt"
@@ -20,6 +21,7 @@ type SablierMiddleware struct {
 
 // New function creates the configuration
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	fmt.Printf("Creating new sablier middleware")
 	req, err := config.BuildRequest(name)
 
 	if err != nil {
