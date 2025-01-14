6e4374accab12e41174f64aa35167d309a6032c2
Geo Saglio <geosaglioo@gmail.com>
2025-01-14 11:41:07 +0100

    feat(traefik plugin) add logs to middleware

--- plugins/traefik/main.go
+++ plugins/traefik/main.go
@@ -62,7 +62,7 @@ func (sm *SablierMiddleware) ServeHTTP(rw http.ResponseWriter, req *http.Request
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	logger.Debug(string(resp.Body.Read()))
+	logger.Debug(fmt.Sprintf("La réponse du service sablier est: %s", resp.Body.Read()))
 	defer resp.Body.Close()
 
 	conditonalResponseWriter := newResponseWriter(rw)
